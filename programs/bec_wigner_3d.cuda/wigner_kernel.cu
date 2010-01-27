22f1e767f9edfa4855e2507ebb368cdf06cbbb4b
Bogdan Opanchuk <bg@bk.ru>
2010-01-27 17:27:51 +1100

    programs\bec_wigner_3d: simplified spectre generators

--- programs/bec_wigner_3d.cuda/wigner_kernel.cu
+++ programs/bec_wigner_3d.cuda/wigner_kernel.cu
@@ -17,100 +17,67 @@ value_type *k_buf;
 // calculation parameters in constant memory, for easy access from kernels
 __constant__ CalculationParameters d_params;
 
+// returns black for index < 0, red for index > 255 * 6 and
+// violet-blue-teal-green-yellow-red for intermediate indices
+dim3 violetToRedSpectre(int index)
+{
+	if(index < 0)
+		return dim3(0, 0, 0);
+	else if(index <= 255) // black -> violet
+		return dim3(index, 0, index);
+	else if(index <= 255 * 2) // violet -> blue
+		return dim3(255 * 2 - index, 0, 255);
+	else if(index <= 255 * 3) // blue -> teal
+		return dim3(0, index - 255 * 2, 255);
+	else if(index <= 255 * 4) // teal -> green
+		return dim3(0, 255, 255 * 4 - index);
+	else if(index <= 255 * 5) // green -> yellow
+		return dim3(index - 255 * 4, 255, 0);
+	else if(index <= 255 * 6) // yellow -> red
+		return dim3(255, 255 * 6 - index, 0);
+	else
+		return dim3(255, 0, 0);
+}
+
+// returns blue for index < 0, red for index > 255 * 3 and
+// blue-white-yellow-red for intermediate indices
+dim3 blueToRedSpectre(int index)
+{
+	if(index < 0)
+		return dim3(0, 0, 0);
+	else if(index <= 255) // blue -> white
+		return dim3(index, index, 255);
+	else if(index <= 255 * 2) // white -> yellow
+		return dim3(255, 255, 255 * 2 - index);
+	else if(index <= 255 * 3) // yellow -> red
+		return dim3(255, 255 * 3 - index, 0);
+	else
+		return dim3(255, 0, 0);
+}
+
+// fills array with spectre colors using given index->color function
+void fillSpectreArray(float4 *array, dim3 (*func)(int), int colors_num)
+{
+	for(int i = 0; i < colors_num; i++)
+	{
+		dim3 color = func(i);
+		array[i].x = (float)color.x / 256;
+		array[i].y = (float)color.y / 256;
+		array[i].z = (float)color.z / 256;
+		array[i].w = 1.0;
+	}
+}
+
+// Prepare 'rainbow' texture which is used to display 2D heightmaps
 void setupRainbow()
 {
-/*
-	int colors_num = 1530;
-	float4 *h_colors = new float4[colors_num];
-
-	int R, G, B;
-
-	for (int i = 0; i < colors_num; i++)
-	{
-		if (i <= 255)
-		// black -> violet
-		{
-			B = i;
-			G = 0;
-			R = i;
-		}
-		else if (i <= 255 * 2)
-		// violet -> blue
-		{
-			B = 255;
-			G = 0;
-			R = 255 * 2 - i;
-		}
-		else if (i <= 255 * 3)
-		// blue -> teal
-		{
-			B = 255;
-			G = i - 255 * 2;
-			R = 0;
-		}
-		else if (i <= 255 * 4)
-		// teal -> green
-		{
-			B = 255 * 4 - i;
-			G = 255;
-			R = 0;
-		}
-		else if (i <= 255 * 5)
-		// green -> yellow
-		{
-			B = 0;
-			G = 255;
-			R = i - 255 * 4;
-		}
-		else if (i <= 255 * 6)
-		// yellow -> red
-		{
-			B = 0;
-			G = 255 * 6 - i;
-			R = 255;
-		}
-
-		h_colors[i].x = (float)R / 256;
-		h_colors[i].y = (float)G / 256;
-		h_colors[i].z = (float)B / 256;
-		h_colors[i].w = 1.0;
-	}
- */
-
+	// int colors_num = 255 * 6;
 	int colors_num = 255 * 3;
+
 	float4 *h_colors = new float4[colors_num];
 
-	int R, G, B;
-
-	for (int i = 0; i < colors_num; i++)
-	{
-		if (i <= 255)
-		// blue -> white
-		{
-			B = 255;
-			G = i;
-			R = i;
-		}
-		else if (i <= 255 * 2)
-		// white -> yellow
-		{
-			B = 255 * 2 - i;
-			G = 255;
-			R = 255;
-		}
-		else if (i <= 255 * 3)
-		// yellow -> red
-		{
-			B = 0;
-			G = 255 * 3 - i;
-			R = 255;
-		}
-
-		h_colors[i].x = (float)R / 256;
-		h_colors[i].y = (float)G / 256;
-		h_colors[i].z = (float)B / 256;
-		h_colors[i].w = 1.0;
-	}
+	// fillSpectreArray(h_colors, violetToRedSpectre, colors_num);
+	fillSpectreArray(h_colors, blueToRedSpectre, colors_num);
 
 	// texture parameters - linear filtering, value range [0.0, 1.0],
 	// f(x) = f(0) for x < 0 and f(x) = f(1) for x > 1
