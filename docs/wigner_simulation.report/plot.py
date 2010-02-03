2deec218e9f1c2cbc3cc2aded24f70d302f683bf
Bogdan Opanchuk <bg@bk.ru>
2010-02-03 17:19:05 +1100

    docs\wigner_simulation: added custom colormap to plot

--- docs/wigner_simulation.report/plot.py
+++ docs/wigner_simulation.report/plot.py
@@ -1,5 +1,13 @@
 import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
 import numpy as np
+
+cmap_file = open("TemperatureMap.tsv")
+cmap_list = []
+for line in cmap_file:
+	elems = [float(x) / 256 for x in line.split("\t")]
+	cmap_list.append(elems)
+cmap = ListedColormap(cmap_list, name="BlueWhiteRed")
 
 def load_xy_data(file_name):
 	f = open(file_name)
@@ -43,8 +51,20 @@ times, z = load_heightmap_data('axial_no_noise.txt')
 z = np.array(z)
 z = z.transpose()
 plt.figure()
-im = plt.imshow(z, interpolation='bilinear', origin='lower', aspect='auto', extent=(0, times[-1], -45, 45))
+im = plt.imshow(z, interpolation='bilinear', origin='lower',
+	aspect='auto', extent=(0, times[-1], -45, 45), cmap=cmap)
 CBI = plt.colorbar(im, orientation='horizontal', shrink=0.8)
 plt.xlabel('Time, ms')
 plt.ylabel('z, $\mu$m')
-plt.savefig('axial.pdf')
+plt.savefig('axial_view.pdf')
+
+times, z = load_heightmap_data('axial_pi_pulse.txt')
+z = np.array(z)
+z = z.transpose()
+plt.figure()
+im = plt.imshow(z, interpolation='bilinear', origin='lower',
+	aspect='auto', extent=(0, times[-1], -45, 45), cmap=cmap)
+CBI = plt.colorbar(im, orientation='horizontal', shrink=0.8)
+plt.xlabel('Time, ms')
+plt.ylabel('z, $\mu$m')
+plt.savefig('axial_pi_pulse.pdf')
