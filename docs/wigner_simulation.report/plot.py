cb96001b5c6da6489d94077eab9e8f293a14fe4a
Bogdan Opanchuk <bg@bk.ru>
2010-02-03 15:49:12 +1100

    docs\wigner_simulation: updated plot.py to render axial view

--- docs/wigner_simulation.report/plot.py
+++ docs/wigner_simulation.report/plot.py
@@ -1,18 +1,50 @@
 import matplotlib.pyplot as plt
-import sys
+import numpy as np
 
-f = open(sys.argv[1])
-times = []
-values = []
-for line in f:
-    elems = line.split(' ')
-    times.append(float(elems[0]))
-    values.append(float(elems[1]))
+def load_xy_data(file_name):
+	f = open(file_name)
+	x = []
+	y = []
+	for line in f:
+		elems = line.split(' ')
+		x.append(float(elems[0]))
+		y.append(float(elems[1]))
+	f.close()
+	return x, y
 
-plt.plot(times, values)
+def load_heightmap_data(file_name):
+	f = open(file_name)
+	times = []
+	arr = []
+	for line in f:
+		elems = line.split(' ')
+		times.append(float(elems[0]))
+		arr.append([float(x) for x in elems[1:]])
+	f.close()
+	return times, arr
+
+# plot visibility
+
+no_noise_x, no_noise_y = load_xy_data('visibility.txt')
+noise_x, noise_y = load_xy_data('visibility_noise.txt')
+
+plt.plot(no_noise_x, no_noise_y, label='Simulation, no quantum noise')
+plt.plot(noise_x, noise_y, label='Simulation, with quantum noise')
+plt.legend()
 plt.xlabel('Time, ms')
 plt.ylabel('Visibility')
 plt.axis([0, 600, 0, 1])
 plt.grid(True)
-plt.savefig(sys.argv[2])
+plt.savefig('visibility.pdf')
 
+# plot evolution
+
+times, z = load_heightmap_data('axial_no_noise.txt')
+z = np.array(z)
+z = z.transpose()
+plt.figure()
+im = plt.imshow(z, interpolation='bilinear', origin='lower', aspect='auto', extent=(0, times[-1], -45, 45))
+CBI = plt.colorbar(im, orientation='horizontal', shrink=0.8)
+plt.xlabel('Time, ms')
+plt.ylabel('z, $\mu$m')
+plt.savefig('axial.pdf')
