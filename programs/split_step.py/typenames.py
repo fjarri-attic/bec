c817ca9580c05756e36c52dea7bd0ae647b2e0ac
Bogdan Opanchuk <bg@bk.ru>
2010-02-21 10:30:19 +1100

    programs/split_step/typenames: added fft precision constant

--- programs/split_step.py/typenames.py
+++ programs/split_step.py/typenames.py
@@ -1,5 +1,10 @@
 import numpy
 
+try:
+	import pycudafft
+except:
+	pass
+
 class _Type:
 	def __init__(self, name, dtype):
 		self.name = name
@@ -8,9 +13,10 @@ class _Type:
 		self.ctr = 'make_' + name
 
 class _Precision:
-	def __init__(self, scalar, complex):
+	def __init__(self, scalar, complex, fft_precision):
 		self.scalar = scalar
 		self.complex = complex
+		self.fft_precision = fft_precision
 
 _single_float = _Type('float', numpy.float32)
 _double_float = _Type('double', numpy.float64)
@@ -18,5 +24,5 @@ _double_float = _Type('double', numpy.float64)
 _single_complex = _Type('float2', numpy.complex64)
 _double_complex = _Type('double2', numpy.complex128)
 
-single_precision = _Precision(_single_float, _single_complex)
-double_precision = _Precision(_double_float, _double_complex)
+single_precision = _Precision(_single_float, _single_complex, pycudafft.SINGLE_PRECISION)
+double_precision = _Precision(_double_float, _double_complex, pycudafft.DOUBLE_PRECISION)
