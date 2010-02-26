b6a1426b05a8937f048f024a38469cb251e67c5e
Bogdan Opanchuk <bg@bk.ru>
2010-02-26 11:59:41 +1100

    programs/split_step: rewritten main program according to new evolution class

--- programs/split_step.py/split_step.py
+++ programs/split_step.py/split_step.py
@@ -1,6 +1,7 @@
 import math
 import matplotlib
 import numpy
+import time
 
 try:
 	import pycuda.autoinit
@@ -9,18 +10,30 @@ try:
 except:
 	pass
 
-from globals import GPUPool
+import matplotlib.pyplot as plt
+
+from globals import *
 from config import Model, precision
 from constants import Constants
-from ground_state import GroundState
+from ground_state import GPEGroundState, ParticleStatistics
+from evolution import TwoComponentBEC
 
 constants = Constants(Model)
 mempool = GPUPool(stub=True)
 gpu = True
 
-gs = GroundState(gpu, precision, constants, mempool)
-gs_gpu = gs.create()
-gs_gpu_h = gs_gpu.get()
+bec = TwoComponentBEC(gpu, precision, constants, mempool)
 
-print numpy.sum(numpy.abs(gs_gpu_h))
+class ParticleNumberPlotter(PairedCalculation):
 
+	def __init__(self, gpu, precision, constants, mempool):
+		PairedCalculation.__init__(self, gpu)
+		self.stats = ParticleStatistics(gpu, precision, constants, mempool)
+
+	def __call__(self, t, a, b):
+		Na = self.stats.countParticles(a)
+		Nb = self.stats.countParticles(b)
+		print t, Na + Nb
+
+pnumber = ParticleNumberPlotter(gpu, precision, constants, mempool)
+bec.runEvolution(0.3, [pnumber], callback_dt=0.005)
