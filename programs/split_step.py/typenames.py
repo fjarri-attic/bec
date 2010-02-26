39d440ab09330399e50800b7759ff737153b4714
Bogdan Opanchuk <bg@bk.ru>
2010-02-26 11:57:29 +1100

    programs/split_step/typenames: added ctype letter to each type

--- programs/split_step.py/typenames.py
+++ programs/split_step.py/typenames.py
@@ -7,10 +7,11 @@ except:
 	DOUBLE_PRECISION = None
 
 class _Type:
-	def __init__(self, name, dtype):
+	def __init__(self, name, dtype, ctype):
 		self.name = name
 		self.dtype = dtype
 		self.nbytes = dtype().nbytes
+		self.ctype = ctype
 		self.ctr = 'make_' + name
 
 class _Precision:
@@ -19,11 +20,11 @@ class _Precision:
 		self.complex = complex
 		self.fft_precision = fft_precision
 
-_single_float = _Type('float', numpy.float32)
-_double_float = _Type('double', numpy.float64)
+_single_float = _Type('float', numpy.float32, "f")
+_double_float = _Type('double', numpy.float64, "d")
 
-_single_complex = _Type('float2', numpy.complex64)
-_double_complex = _Type('double2', numpy.complex128)
+_single_complex = _Type('float2', numpy.complex64, "d")
+_double_complex = _Type('double2', numpy.complex128, None)
 
 single_precision = _Precision(_single_float, _single_complex, SINGLE_PRECISION)
 double_precision = _Precision(_double_float, _double_complex, DOUBLE_PRECISION)
