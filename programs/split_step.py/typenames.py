3f447226d7deba373f3fc334f4f28fcfd64e7085
Bogdan Opanchuk <mantihor@gmail.com>
2010-03-24 01:48:44 +1100

    programs/split_step/typenames: removed unused field 'cname'

--- programs/split_step.py/typenames.py
+++ programs/split_step.py/typenames.py
@@ -1,11 +1,10 @@
 import numpy
 
 class _Type:
-	def __init__(self, name, dtype, ctype):
+	def __init__(self, name, dtype):
 		self.name = name
 		self.dtype = dtype
 		self.nbytes = dtype().nbytes
-		self.ctype = ctype
 		self.ctr = '(' + name + ')'
 
 class _Precision:
@@ -13,11 +12,11 @@ class _Precision:
 		self.scalar = scalar
 		self.complex = complex
 
-_single_float = _Type('float', numpy.float32, "f")
-_double_float = _Type('double', numpy.float64, "d")
+_single_float = _Type('float', numpy.float32)
+_double_float = _Type('double', numpy.float64)
 
-_single_complex = _Type('float2', numpy.complex64, "d")
-_double_complex = _Type('double2', numpy.complex128, None)
+_single_complex = _Type('float2', numpy.complex64)
+_double_complex = _Type('double2', numpy.complex128)
 
 single_precision = _Precision(_single_float, _single_complex)
 double_precision = _Precision(_double_float, _double_complex)
