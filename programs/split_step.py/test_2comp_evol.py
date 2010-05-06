1f1a0f942c0a440872789aed943e485cec2c0dff
Bogdan Opanchuk <mantihor@gmail.com>
2010-05-06 11:43:53 +1000

    programs/split_step/test_2comp_evol: using new evolution graph class

--- programs/split_step.py/test_2comp_evol.py
+++ programs/split_step.py/test_2comp_evol.py
@@ -10,7 +10,7 @@ import typenames
 
 from collectors import *
 
-from datahelpers import XYData, HeightmapData, XYPlot, HeightmapPlot
+from datahelpers import XYData, HeightmapData, XYPlot, HeightmapPlot, EvolutionPlot
 
 m = Model()
 constants = Constants(m)
@@ -18,7 +18,7 @@ env = Environment(True, typenames.single_precision, constants)
 bec = TwoComponentBEC(env)
 
 e = EqualParticleNumberCondition(env)
-t_to_equality = bec.runEvolution(0.05, [e])
+t_to_equality = bec.runEvolution(0.2, [e])
 env.synchronize()
 
 print t_to_equality
@@ -28,25 +28,27 @@ p = ParticleNumberCollector(env, verbose=True, do_pulse=False)
 sc = SliceCollector(env)
 sp = SurfaceProjectionCollector(env)
 
-bec._pulse.halfPi(bec._a, bec._b)
-bec.runEvolution(0.1, [a, p], callback_dt=0.001)
+bec._pulse.apply(bec._a, bec._b, 3.0 * math.pi / 2.0, 0)
+bec.runEvolution(0.199, [sp], callback_dt=0.01)
 env.synchronize()
 
-sp(0, bec._a, bec._b)
 times, a_xy, a_yz, b_xy, b_yz = sp.getData()
-a_data = HeightmapData("1 component", a_yz[0].transpose(), xmin=-env.constants.zmax, xmax=env.constants.zmax,
-	xname="Z, $\\mu$m", yname="Y, $\\mu$m",
-	ymin=-env.constants.ymax, ymax=env.constants.ymax, zmin=0)
-HeightmapPlot(a_data).save('test_a.pdf')
 
-b_data = HeightmapData("2 component", b_yz[0].transpose(), xmin=-env.constants.zmax, xmax=env.constants.zmax,
-	xname="Z, $\\mu$m", yname="Y, $\\mu$m",
-	ymin=-env.constants.ymax, ymax=env.constants.ymax, zmin=0)
-HeightmapPlot(b_data).save('test_b.pdf')
+#times, picture = a.getData()
+#pr = HeightmapData("test", picture, xmin=0, xmax=100,
+#	ymin=-env.constants.zmax, ymax=env.constants.zmax, zmin=-1,
+#	zmax=1, xname="Time, ms", yname="z, $\\mu$m", zname="Spin projection")
+#pr = HeightmapPlot(pr)
+#pr.save('test.pdf')
 
-times, picture = a.getData()
-pr = HeightmapData("test", picture, xmin=0, xmax=100,
-	ymin=-env.constants.zmax, ymax=env.constants.zmax, zmin=-1,
-	zmax=1, xname="Time, ms", yname="z, $\\mu$m", zname="Spin projection")
-pr = HeightmapPlot(pr)
-pr.save('test.pdf')
+times = [str(int(x * 1000 + 0.5)) for x in times]
+
+for name, dataset in (('testa.pdf', a_yz), ('testb.pdf', b_yz)):
+	hms = []
+	for t, hm in zip(times, dataset):
+		hms.append(HeightmapData(t, hm.transpose(),
+			xmin=-env.constants.zmax, xmax=env.constants.zmax,
+			ymin=-env.constants.ymax, ymax=env.constants.ymax,
+			zmin=0, zmax=400))
+
+	EvolutionPlot(hms, shape=(5, 4)).save(name)
