3fff0404133e8b806d344f94c8d3edb7723dc2d7
Bogdan Opanchuk <mantihor@gmail.com>
2010-05-12 00:06:07 +1000

    programs/split_step/test_2comp_gs: fixed according to new interface

--- programs/split_step.py/test_2comp_gs.py
+++ programs/split_step.py/test_2comp_gs.py
@@ -5,34 +5,32 @@ import math
 from globals import Environment
 from model import Model
 from constants import Constants
-from evolution import TwoComponentBEC
+from evolution import TwoComponentEvolution
 from ground_state import GPEGroundState
-from collectors import SurfaceProjectionCollector
-import typenames
 
 from collectors import *
 
 from datahelpers import XYData, HeightmapData, XYPlot, HeightmapPlot
 
-m = Model()
-constants = Constants(m)
-env = Environment(False, typenames.single_precision, constants)
+# preparation
+env = Environment(gpu=False)
+constants = Constants(Model(N=150000), double_precision=False)
+gs = GPEGroundState(env, constants)
+sp = SliceCollector(env, constants, do_pulse=False)
 
-gs = GPEGroundState(env)
+# experiment
+cloud = gs.create(two_component=True)
+sp(0, cloud)
 
-a, b = gs.create(two_component=True)
-sp = SliceCollector(env)
-
-sp(0, a, b)
+# render
 times, a_xy, a_yz, b_xy, b_yz = sp.getData()
 
-
-a_data = HeightmapData("1 component", a_yz[0].transpose(), xmin=-env.constants.zmax, xmax=env.constants.zmax,
+a_data = HeightmapData("1 component", a_yz[0].transpose(), xmin=-constants.zmax, xmax=constants.zmax,
 	xname="Z, $\\mu$m", yname="Y, $\\mu$m",
-	ymin=-env.constants.ymax, ymax=env.constants.ymax, zmin=0)
+	ymin=-constants.ymax, ymax=constants.ymax, zmin=0)
 HeightmapPlot(a_data).save('test_a.pdf')
 
-b_data = HeightmapData("2 component", b_yz[0].transpose(), xmin=-env.constants.zmax, xmax=env.constants.zmax,
+b_data = HeightmapData("2 component", b_yz[0].transpose(), xmin=-constants.zmax, xmax=constants.zmax,
 	xname="Z, $\\mu$m", yname="Y, $\\mu$m",
-	ymin=-env.constants.ymax, ymax=env.constants.ymax, zmin=0)
+	ymin=-constants.ymax, ymax=constants.ymax, zmin=0)
 HeightmapPlot(b_data).save('test_b.pdf')
