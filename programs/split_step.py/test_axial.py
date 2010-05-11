b2766ade21fb4dfd1375e7aa55a703c13e53327b
Bogdan Opanchuk <mantihor@gmail.com>
2010-05-11 23:36:47 +1000

    programs/split_step/test_axial: fixed according to new interface

--- programs/split_step.py/test_axial.py
+++ programs/split_step.py/test_axial.py
@@ -5,29 +5,34 @@ import math
 from globals import Environment
 from model import Model
 from constants import Constants
-from evolution import TwoComponentBEC
-import typenames
+from evolution import TwoComponentEvolution
+from ground_state import GPEGroundState
 
 from collectors import *
 
 from datahelpers import XYData, HeightmapData, XYPlot, HeightmapPlot
 
-m = Model()
-constants = Constants(m)
-env = Environment(False, typenames.double_precision, constants)
-bec = TwoComponentBEC(env)
+# preparation
+env = Environment(gpu=False)
+constants = Constants(Model(N=150000), double_precision=False)
+gs = GPEGroundState(env, constants)
+evolution = TwoComponentEvolution(env, constants)
+pulse = Pulse(env, constants)
+a = AxialProjectionCollector(env, constants)
 
-a = AxialProjectionCollector(env)
+# experiment
+cloud = gs.create()
+pulse.halfPi(cloud)
 t1 = time.time()
-bec.runEvolution(0.099, [a], callback_dt=0.001)
+evolution.run(cloud, time=0.3, callbacks=[a], callback_dt=0.005)
 env.synchronize()
 t2 = time.time()
 print "Time spent: " + str(t2 - t1) + " s"
 
 times, picture = a.getData()
 
-pr = HeightmapData("test", picture, xmin=0, xmax=100,
-	ymin=-env.constants.zmax, ymax=env.constants.zmax, zmin=-1,
+pr = HeightmapData("test", picture, xmin=0, xmax=300,
+	ymin=-constants.zmax, ymax=constants.zmax, zmin=-1,
 	zmax=1, xname="Time, ms", yname="z, $\\mu$m", zname="Spin projection")
 pr = HeightmapPlot(pr)
 pr.save('test.pdf')
