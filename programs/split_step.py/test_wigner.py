cf6cbfe4255d3795c87b78c3d57f6d84f710d376
Bogdan Opanchuk <mantihor@gmail.com>
2010-05-23 00:26:25 +1000

    programs/split_step/test_wigner: testing script for Wigner statistics

---
+++ programs/split_step.py/test_wigner.py
@@ -0,0 +1,41 @@
+import numpy
+import time
+import math
+
+from globals import Environment
+from model import Model
+from constants import Constants
+from evolution import TwoComponentEvolution, Pulse
+from ground_state import GPEGroundState
+
+from collectors import *
+
+from datahelpers import XYData, HeightmapData, XYPlot, HeightmapPlot
+
+constants = Constants(Model(N=30000), double_precision=True)
+env = Environment(gpu=False)
+evolution = TwoComponentEvolution(env, constants)
+a = VisibilityCollector(env, constants, verbose=True)
+b = ParticleNumberCollector(env, constants, verbose=True)
+
+gs = GPEGroundState(env, constants)
+pulse = Pulse(env, constants)
+
+cloud = gs.createCloud()
+cloud.a.toWigner()
+cloud.b.toWigner()
+
+pulse.halfPi(cloud)
+t1 = time.time()
+evolution.run(cloud, 0.399, callbacks=[a, b], callback_dt=0.01)
+env.synchronize()
+t2 = time.time()
+print "Time spent: " + str(t2 - t1) + " s"
+
+times, vis = a.getData()
+
+vis = XYData("GPU, noise with equilibration", times, vis, ymin=0, ymax=1,
+	xname="Time, ms", yname="Visibility")
+vis.save('test.yaml')
+vis = XYPlot([vis])
+vis.save('test.pdf')
