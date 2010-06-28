fce2f7c1bcc1b0abb8422eab619d24b76c60da00
Bogdan Opanchuk <mantihor@gmail.com>
2010-06-28 23:59:46 +1000

    programs/beclab/examples/evolution_graph: fixed according to changed interface

--- programs/beclab.py/examples/evolution_graph.py
+++ programs/beclab.py/examples/evolution_graph.py
@@ -2,44 +2,47 @@ import numpy
 import time
 import math
 
-from globals import Environment
-from model import Model
-from constants import Constants
-from evolution import TwoComponentEvolution
-from ground_state import GPEGroundState
+from beclab import *
 
-from collectors import *
+def testEvolutionGraph(gpu):
+	# preparation
+	env = Environment(gpu=gpu)
+	constants = Constants(Model(N=150000), double_precision=False if gpu else True)
+	gs = GPEGroundState(env, constants)
+	evolution = SplitStepEvolution(env, constants)
+	pulse = Pulse(env, constants)
+	a = SurfaceProjectionCollector(env, constants, pulse=pulse)
 
-from datahelpers import XYData, HeightmapData, XYPlot, HeightmapPlot, EvolutionPlot
+	# experiment
+	cloud = gs.createCloud()
+	pulse.apply(cloud, theta=0.5*math.pi)
+	t1 = time.time()
+	evolution.run(cloud, time=0.12, callbacks=[a], callback_dt=0.005)
+	env.synchronize()
+	t2 = time.time()
+	print "Time spent: " + str(t2 - t1) + " s"
 
-# preparation
-env = Environment(gpu=False)
-constants = Constants(Model(N=150000), double_precision=False)
-gs = GPEGroundState(env, constants)
-evolution = TwoComponentEvolution(env, constants)
-pulse = Pulse(env, constants)
-a = SurfaceProjectionCollector(env, constants)
+	# render
+	times, a_xy, a_yz, b_xy, b_yz = a.getData()
 
-# experiment
-cloud = gs.create()
-pulse.halfPi(cloud)
-t1 = time.time()
-evolution.run(cloud, time=0.399, callbacks=[a], callback_dt=0.01)
-env.synchronize()
-t2 = time.time()
-print "Time spent: " + str(t2 - t1) + " s"
+	times = [str(int(x * 1000 + 0.5)) for x in times]
 
-# render
-times, a_xy, a_yz, b_xy, b_yz = a.getData()
+	def constructPlot(dataset):
+		hms = []
+		for t, hm in zip(times, dataset):
+			hms.append(HeightmapData(t, hm.transpose(),
+				xmin=-constants.zmax, xmax=constants.zmax,
+				ymin=-constants.ymax, ymax=constants.ymax,
+				zmin=0, zmax=400))
 
-times = [str(int(x * 1000 + 0.5)) for x in times]
+		return EvolutionPlot(hms, shape=(6, 4))
 
-for name, dataset in (('testa.pdf', a_yz), ('testb.pdf', b_yz)):
-	hms = []
-	for t, hm in zip(times, dataset):
-		hms.append(HeightmapData(t, hm.transpose(),
-			xmin=-constants.zmax, xmax=constants.zmax,
-			ymin=-constants.ymax, ymax=constants.ymax,
-			zmin=0, zmax=400))
+	return constructPlot(a_yz), constructPlot(b_yz)
 
-	EvolutionPlot(hms, shape=(8, 5)).save(name)
+p1, p2 = testEvolutionGraph(gpu=False)
+p1.save("evolution_cpu_a.pdf")
+p2.save("evolution_cpu_b.pdf")
+
+p1, p2 = testEvolutionGraph(gpu=True)
+p1.save("evolution_gpu_a.pdf")
+p2.save("evolution_gpu_b.pdf")
